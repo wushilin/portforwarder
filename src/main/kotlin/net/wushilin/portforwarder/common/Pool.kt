5e1dd84f0ae8910b9b3d2c9ed4d90789f8a6242a
Wu Shilin <wushilin.sg@gmail.com>
2021-07-26 12:54:16 +0800

    local

--- src/main/kotlin/net/wushilin/portforwarder/common/Pool.kt
+++ src/main/kotlin/net/wushilin/portforwarder/common/Pool.kt
@@ -1,10 +1,15 @@
 package net.wushilin.portforwarder.common
 
+import java.math.BigDecimal
+import java.math.RoundingMode
 import java.util.*
 
 class Pool<T>(private val size: Int, val supplier:()->T, val cleaner:((T)->Unit)? = null) {
     private val buffer = LinkedList<T>()
+    private var hardAcquireCount = 0L
+    private var acquireCount = 0L
     fun acquire():T {
+        acquireCount++
         return if(buffer.size == 0) {
             hardAcquire()
         } else {
@@ -13,12 +18,34 @@ class Pool<T>(private val size: Int, val supplier:()->T, val cleaner:((T)->Unit)
     }
 
     fun release(value:T) {
-        if(buffer.size >= size) {
-            return
+        try {
+            if (buffer.size >= size) {
+                return
+            }
+            buffer.addLast(value)
+        } finally {
+            cleaner?.let { it(value) }
         }
-        buffer.addLast(value)
     }
+
+    fun size():Int {
+        return buffer.size
+    }
+
     private fun hardAcquire():T {
+        hardAcquireCount++
         return supplier()
+    }
+
+    fun hardAcquireCount():Long {
+        return hardAcquireCount
+    }
+
+    fun hitRate():Double {
+        if(acquireCount == 0L) {
+            return 0.0;
+        }
+        return BigDecimal(acquireCount - hardAcquireCount).divide(BigDecimal(acquireCount),
+            2, RoundingMode.HALF_UP).toDouble()
     }
 }
